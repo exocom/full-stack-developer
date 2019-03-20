848e19b0a8add8afba1743fe92210c618948ab69
Kalarrs Topham <k-dawg-klakr@hotmail.com>
2019-03-20 14:04:48 -0700

    Added Update topping request.

--- toppings/src/models/request.ts
+++ toppings/src/models/request.ts
@@ -1,9 +1,17 @@
-import {IsDefined, IsEnum, Matches, MaxLength, MinLength, ValidateNested} from 'class-validator';
+import {IsDefined, IsEnum, Matches, MaxLength, MinLength, ValidateNested, ValidationOptions} from 'class-validator';
 import {Transform, Type} from 'class-transformer';
 import {ToppingType} from './toppings';
 import {ObjectId} from 'mongodb';
 
 export const dataUrlRegExp = /^data:(.*?\/(.*?));(.*$)/;
+
+const enumCustomErrorMessage: ValidationOptions = {
+  message: ({property, constraints}) => {
+    const [toppingTypeEnum] = constraints;
+    const values = Object.values(toppingTypeEnum).join(', ');
+    return `${property} must be a valid enum value: ${values}`;
+  }
+};
 
 export class ImageDataUrl {
   @IsDefined()
@@ -17,13 +25,7 @@ export class CreateToppingBody {
   @MaxLength(35)
   name: string;
 
-  @IsEnum(ToppingType, {
-    message: ({property, constraints}) => {
-      const [toppingTypeEnum] = constraints;
-      const values = Object.values(toppingTypeEnum).join(', ');
-      return `${property} must be a valid enum value: ${values}`;
-    }
-  })
+  @IsEnum(ToppingType, enumCustomErrorMessage)
   type: ToppingType;
 
   @IsDefined()
@@ -35,4 +37,29 @@ export class CreateToppingBody {
 export class DeleteToppingPathParameters {
   @Transform(value => ObjectId(value), {toClassOnly: true})
   toppingId: ObjectId;
+}
+
+
+export class UpdateToppingPathParameters {
+  @Transform(value => ObjectId(value), {toClassOnly: true})
+  toppingId: ObjectId;
+}
+
+export class UpdateToppingBody {
+  @IsDefined()
+  @Transform(value => ObjectId(value), {toClassOnly: true})
+  id: ObjectId;
+
+  @IsDefined()
+  @MinLength(3)
+  @MaxLength(35)
+  name: string;
+
+  @IsEnum(ToppingType, enumCustomErrorMessage)
+  type: ToppingType;
+
+  @IsDefined()
+  @ValidateNested()
+  @Type(() => ImageDataUrl)
+  image: ImageDataUrl;
 }
