3a20c2df03120a75f90f623fdc88b71cd9c52b8a
Kalarrs Topham <k-dawg-klakr@hotmail.com>
2019-03-22 00:29:00 -0700

    Added firebase hosting.

--- frontend/src/app/pages/home/home.page.ts
+++ frontend/src/app/pages/home/home.page.ts
@@ -1,4 +1,4 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {NesControllerBottomSheetComponent} from '../../bottom-sheets/nes-controller/nes-controller-bottom-sheet.component';
 import {MatBottomSheet} from '@angular/material';
 
@@ -7,9 +7,13 @@ import {MatBottomSheet} from '@angular/material';
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss']
 })
-export class HomePage {
+export class HomePage implements OnInit {
 
   constructor(private bottomSheet: MatBottomSheet) {
+  }
+
+  ngOnInit(): void {
+    setTimeout(() => this.openBottomSheet(), 500);
   }
 
   openBottomSheet(): void {
